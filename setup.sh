4fa7f8e8f3f8ec0cf58c91ae2656fca24e394b7e
jagoanneon01 <89620451+jagoanneon01@users.noreply.github.com>
2022-04-19 02:21:18 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -59,7 +59,11 @@ echo "" | tee -a log-install.txt
 echo "----------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
-echo "   - OpenSSH                 : 22, "  | tee -a log-install.txt
+echo "   - OpenSSH                 : 22, 2253"  | tee -a log-install.txt
+echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 990"  | tee -a log-install.txt
+echo "   - Stunnel5                : 2443, 445"  | tee -a log-install.txt
+echo "   - Dropbear                : 2443, 109, 143"  | tee -a log-install.txt
+echo "   - Squid Proxy             : 3128, 8080"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
 echo "   - Nginx                   : 89"  | tee -a log-install.txt
 echo "   - XRAYS Vmess TLS         : 443"  | tee -a log-install.txt
@@ -67,8 +71,12 @@ echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - XRAYS Vless TLS         : 443"  | tee -a log-install.txt
 echo "   - XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
 echo "   - XRAYS Trojan            : 2083"  | tee -a log-install.txt
-echo "   - XRAYS VMESS GRPC        : 2086"  | tee -a log-install.txt
+echo "   - XRAYS VMESS GRPC        : 2080"  | tee -a log-install.txt
 echo "   - XRAYS VLESS GRPC        : 2096"  | tee -a log-install.txt
+echo "   - CloudFront Websocket    : "  | tee -a log-install.txt
+echo "   - Websocket TLS           : 2443"  | tee -a log-install.txt
+echo "   - Websocket None TLS      : 2082"  | tee -a log-install.txt
+echo "   - Websocket Ovpn          : 2086"  | tee -a log-install.txt
 echo "   - TrojanGo                : 2087"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
