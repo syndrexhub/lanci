683669464d2851458d74b59a0263b4dc0145ae77
jagoanneon01 <89620451+jagoanneon01@users.noreply.github.com>
2022-04-21 03:17:35 +0700

    Update addtrojanfull.sh

--- trojango/addtrojan.sh
+++ trojango/addtrojan.sh
@@ -25,6 +25,7 @@ echo "WhatsApp   : 083857684916"
 exit 0
 fi
 clear
+uuid=$(cat /etc/trojan-go/uuid.txt)
 source /var/lib/crot/ipvps.conf
 if [[ "$IP" = "" ]]; then
 domain=$(cat /etc/xray/domain)
@@ -32,10 +33,10 @@ else
 domain=$IP
 fi
 read -rp "Masukkan Bug: " -e bug
-tr="$(cat ~/log-install.txt | grep -w "Trojan" | cut -d: -f2|sed 's/ //g')"
+trgo="$(cat ~/log-install.txt | grep -w "TrojanGo" | cut -d: -f2|sed 's/ //g')"
 until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 		read -rp "Password : " -e user
-		user_EXISTS=$(grep -w $user /etc/xray/config.json | wc -l)
+		user_EXISTS=$(grep -w $user /etc/trojan-go/akun.conf | wc -l)
 
 		if [[ ${user_EXISTS} == '1' ]]; then
 			echo ""
@@ -44,48 +45,41 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 		fi
 	done
 read -p "Expired (Days) : " masaaktif
-hariini=`date -d "0 days" +"%Y-%m-%d"`
+sed -i '/"'""$uuid""'"$/a\,"'""$user""'"' /etc/trojan-go/config.json
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-sed -i '/#xray-trojan$/a\#&# '"$user $exp"'\
-},{"password": "'""$user""'","email": "'""$user""'"' /etc/xray/config.json
-sed -i '/#xray-trojan-wsnone$/a\#### '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
-sed -i '/#xray-trojan-wstls$/a\#### '"$user $exp"'\
-},{"id": "'""$uuid""'","email": "'""$user""'"' /etc/xray/config.json
-systemctl restart xray.service
-trojanlink="trojan://${user}@${domain}:${tr}/${bug}"
-trojanlinkk="trojan://${user}@${MYIP}:${tr}/${bug}"
-trojanwstls="trojan://${uuid}@${domain}:8443?path=/Ronggolawe&security=tls&encryption=none&type=ws#${user}"
-trojanwsnone="trojan://${uuid}@${domain}:8880?path=/Ronggolawe&encryption=none&type=ws#${user}"
-trojanwss="trojan://${user}@${domain}:8880/?sni=none&type=ws&host=${domain}&path=/Ronggolawe&encryption=none"
-trojanwsss="trojan://${user}@${domain}:8443/?sni=${domain}&type=ws&host=${domain}&path=/Ronggolawe&security=tls&encryption=none"
-service cron restart
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+echo -e "### $user $exp" >> /etc/trojan-go/akun.conf
+systemctl restart trojan-go.service
+link="trojan-go://${user}@${bug}:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=/Ronggolawe&encryption=none#$user"
+link1="trojan://${user}@${bug}:${trgo}/?sni=${domain}&type=ws&host=${domain}&path=/Ronggolawe&encryption=none#$user"
+link2="trojan://${user}@${bug}:${trgo}/?sni=&type=ws&host=${domain}&path=/Ronggolawe&encryption=none#$user"
 clear
 echo -e ""
 echo -e "════════════════" | lolcat
-echo -e "=•=•-XRAYS/TROJAN-=•=•"
+echo -e "${RED}=====-TROJAN-GO-====${NC}"
 echo -e "════════════════" | lolcat
-echo -e "Remarks  : ${user}"
-echo -e "IP/Host  : ${MYIP}"
-echo -e "Address  : ${domain}"
-echo -e "Port     : ${tr}"
-echo -e "Key      : ${user}"
-echo -e "Bug     : ${bug}
-echo -e "Created  : $hariini"
-echo -e "Expired  : $exp"
+echo -e "Remarks    : ${user}"
+echo -e "IP/Host    : ${MYIP}"
+echo -e "Address    : ${domain}"
+echo -e "Port       : ${trgo}"
+echo -e "Key        : ${user}"
+echo -e "Encryption : none"
+echo -e "Bug.       : ${bug}
+echo -e "Path       : /trojango"
+echo -e "Created    : $hariini"
+echo -e "Expired    : $exp"
 echo -e "════════════════" | lolcat
-echo -e "Link TR  : ${trojanlink}"
+echo -e "Link Trojan-WsNone  : 
+echo -e ""
+echo -e " ${link2}"
 echo -e "════════════════" | lolcat
-echo -e "Link TR IP  : ${trojanlinkk}"
+echo -e "Link Trojan-WsTLS  : 
+echo -e ""
+echo -e " ${link1}"
 echo -e "════════════════" | lolcat
-echo -e "Link TR wss : "
-echo -e " ${trojanwstls}"
-echo -e "════════" | lolcat
-echo -e " ${trojanwsnone}"
-echo -e "════════" | lolcat
-echo -e " ${trojanwss}"
-echo -e "════════" | lolcat
-echo -e " ${trojanwsss}"
+echo -e "Link Trojan-go  : 
+echo -e ""
+echo -e " ${link}"
 echo -e "════════════════" | lolcat
 echo -e "${RED}AutoScriptSSH By Ronggolawe${NC}"
 echo -e "════════════════" | lolcat
