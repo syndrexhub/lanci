04700a70b2fd410515ac08c88613aefedae259bf
jagoanneon01 <89620451+jagoanneon01@users.noreply.github.com>
2022-04-21 02:52:55 +0700

    First Commit

---
+++ delssh.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# AutoScriptSSH By JAGOANNEON
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+MYIP=$(wget -qO- ipinfo.io/ip);
+echo "Checking VPS"
+sleep 1
+echo "Cek Izin Akses Script"
+IZIN=$( curl http://akses.jagoanneon-premium.xyz:81/akses | grep $MYIP )
+if [ $MYIP = $IZIN ]; then
+echo -e "${GREEN}Akses Di Izinkan...${NC}"
+else
+echo -e "${RED}VPS tidak diijinkan${NC}";
+echo "Kontak Admin Untuk Mendapatkan Akses Script"
+echo "Facebook   : Generasi Ronggolawe Tuban"
+echo "WhatsApp   : 083857684916"
+exit 0
+fi
+clear
+read -p "Username SSH to Delete : " Pengguna
+
+if getent passwd $Pengguna > /dev/null 2>&1; then
+        userdel $Pengguna
+        echo -e "Username $Pengguna Telah Di Hapus"
+else
+        echo -e "Failure: Username $Pengguna Tidak Ada CoK"
+fi
