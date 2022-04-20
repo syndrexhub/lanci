3c27ab64c7bb63543e3e7308da014eb42d04c62d
jagoanneon01 <89620451+jagoanneon01@users.noreply.github.com>
2022-04-21 05:12:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -44,7 +44,7 @@ wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/xray/ins-xray.sh
 # Trojango
 wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/trojango/ins-trojango.sh && chmod +x ins-trojango.sh && sed -i -e 's/\r$//' ins-trojango.sh && ./ins-trojango.sh
 # install xray grpc
-wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/grpc/fb-grpc.sh && chmod +x fb-grpc.sh && sed -i -e 's/\r$//' fb-grpc.sh && screen -S grpc ./fb-grpc.sh
+wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/grpc/grpc.sh && chmod +x grpc.sh && sed -i -e 's/\r$//' grpc.sh && screen -S grpc ./grpc.sh
 #
 #install websocket
 wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/websocket/edu.sh && chmod +x edu.sh && sed -i -e 's/\r$//' edu.sh && screen -S websocket ./edu.sh
@@ -53,8 +53,8 @@ wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/websocket/edu.sh
 rm -f /root/startcomand.sh
 rm -f /root/ins-xray.sh
 rm -f /root/ins-trojango.sh
-rm -f /root/fb-grpc.sh
-rm -f /root/websocket.sh
+rm -f /root/grpc.sh
+rm -f /root/edu.sh
 history -c
 echo "1.2" > /home/ver
 echo " "
@@ -66,7 +66,7 @@ echo ""  | tee -a log-install.txt
 echo "   >>> Service & Port"  | tee -a log-install.txt
 echo "   - OpenSSH                 : 22, 2253"  | tee -a log-install.txt
 echo "   - OpenVPN                 : TCP 1194, UDP 2200, SSL 990"  | tee -a log-install.txt
-echo "   - Stunnel5                : 2443, 445"  | tee -a log-install.txt
+echo "   - Stunnel4                : 2443, 445"  | tee -a log-install.txt
 echo "   - Dropbear                : 2443, 109, 143"  | tee -a log-install.txt
 echo "   - Squid Proxy             : 3128, 8080"  | tee -a log-install.txt
 echo "   - Badvpn                  : 7100, 7200, 7300"  | tee -a log-install.txt
@@ -76,12 +76,12 @@ echo "   - XRAYS Vmess None TLS    : 80"  | tee -a log-install.txt
 echo "   - XRAYS Vless TLS         : 443"  | tee -a log-install.txt
 echo "   - XRAYS Vless None TLS    : 80"  | tee -a log-install.txt
 echo "   - XRAYS Trojan            : 2083"  | tee -a log-install.txt
-echo "   - XRAYS VMESS GRPC        : 2080"  | tee -a log-install.txt
-echo "   - XRAYS VLESS GRPC        : 2096"  | tee -a log-install.txt
+echo "   - XRAYS VMESS GRPC        : 8443"  | tee -a log-install.txt
+echo "   - XRAYS VLESS GRPC        : 8880"  | tee -a log-install.txt
 echo "   - CloudFront Websocket    : "  | tee -a log-install.txt
 echo "   - Websocket TLS           : 2443"  | tee -a log-install.txt
 echo "   - Websocket None TLS      : 2082"  | tee -a log-install.txt
-echo "   - Websocket Ovpn          : 2086"  | tee -a log-install.txt
+echo "   - Websocket Ovpn          : 2096"  | tee -a log-install.txt
 echo "   - TrojanGo                : 2087"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
 echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
