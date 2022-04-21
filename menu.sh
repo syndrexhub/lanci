0ca5fe817fe1d2529edd7c6f4169eee045fbbafc
jagoanneon01 <89620451+jagoanneon01@users.noreply.github.com>
2022-04-22 04:33:58 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -5,67 +5,68 @@ echo -e " SEDANG PROSES OM HARAP BERSABAR"
 sleep 2
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━ Menu ━━━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "* menu              : Menampilkan Daftar Perintah"
-echo -e "* Attention         : Silahkan Ketik Perintah Sesuai Dengan List Di Menu"
+echo -e "* menu              : Menampilkan Daftar Perintah" | lolcat
+echo -e "* Attention         : Silahkan Ketik Perintah Sesuai Dengan List Di Menu" | lolcat
 echo -e "* Lokasimu          : $WKT " | lolcat
+echo -e "* JAM               : date |lolcat
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━ TRIAL-VPN ━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "* trial-ssh         : Membuat Trial Akun SSH"
-echo -e "* trial-vmess       : Membuat Trial Akun Vmess"
-echo -e "* trial-vless       : Membuat Trial Akun Vless"
-echo -e "* trial-trojan      : Membuat Trial Akun trojan"
-echo -e "* trial-trojango    : Membuat Trial Akun trojan-go"
-echo -e "* trial-grpcc       : Membuat Trial Akun grpc"
+echo -e "* trial-ssh         : Membuat Trial Akun SSH" | lolcat
+echo -e "* trial-vmess       : Membuat Trial Akun Vmess" | lolcat
+echo -e "* trial-vless       : Membuat Trial Akun Vless" | lolcat
+echo -e "* trial-trojan      : Membuat Trial Akun trojan" | lolcat
+echo -e "* trial-trojango    : Membuat Trial Akun trojan-go" | lolcat
+echo -e "* trial-grpcc       : Membuat Trial Akun grpc" | lolcat
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━ SSH-OVPN ━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "* addssh            : Membuat Akun SSH"
-echo -e "* delssh            : Menghapus Akun SSH Secara Paksa"
-echo -e "* delexp            : Menghapus Akun SSH Yang Sudah EXP"
-echo -e "* member            : Cek Member SSH"
-echo -e "* cekssh            : Cek User Login SSH"
-echo -e "* renewssh          : Memperpanjang Akun SS+"
+echo -e "* addssh            : Membuat Akun SSH" | lolcat
+echo -e "* delssh            : Menghapus Akun SSH Secara Paksa" | lolcat
+echo -e "* delexp            : Menghapus Akun SSH Yang Sudah EXP" | lolcat
+echo -e "* member            : Cek Member SSH" | lolcat
+echo -e "* cekssh            : Cek User Login SSH" | lolcat
+echo -e "* renewssh          : Memperpanjang Akun SSH" | lolcat
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━ TROJAN-GO ━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "* addtrgo           : Membuat Akun Trojan-GO"
-echo -e "* deltrgo           : Menghapus Akun Trojan-GO"
-echo -e "* cektrgo           : Cek User Login Trojan-GO"
-echo -e "* renewtrgo         : Memperpanjang Masa Aktif Akun Trojan-GO"
+echo -e "* addtrgo           : Membuat Akun Trojan-GO" | lolcat
+echo -e "* deltrgo           : Menghapus Akun Trojan-GO" | lolcat
+echo -e "* cektrgo           : Cek User Login Trojan-GO" | lolcat
+echo -e "* renewtrgo         : Memperpanjang Masa Aktif Akun Trojan-GO" | lolcat
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━ GRPC ━━━━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "* addgrpc        : Membuat Akun GRPC"
-echo -e "* delgrpc        : Menghapus Akun GRPC"
-echo -e "* cekgrpc        : Memperpanjang Masa Aktif Akun GRPC"
-echo -e "* renewgrpc      : Cek User Login GRPC"
+echo -e "* addgrpc        : Membuat Akun GRPC" | lolcat
+echo -e "* delgrpc        : Menghapus Akun GRPC" | lolcat
+echo -e "* cekgrpc        : Memperpanjang Masa Aktif Akun GRPC" | lolcat
+echo -e "* renewgrpc      : Cek User Login GRPC" | lolcat
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━ VMESS ━━━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "* addvmess          : Membuat Akun V2RAY Vmess Websocket"
-echo -e "* delvmess          : Menghapus Akun V2RAY Vmess Websocket"
-echo -e "* renewvmess        : Memperpanjang Masa Aktif Akun Vmess"
-echo -e "* cekvmess          : Cek User Login V2RAY"
-echo -e "* cert2vray         : Memperbarui Sertifikat Akun V2RAY [ Apabila Ganti Domain]"
+echo -e "* addvmess          : Membuat Akun V2RAY Vmess Websocket" | lolcat
+echo -e "* delvmess          : Menghapus Akun V2RAY Vmess Websocket" | lolcat
+echo -e "* renewvmess        : Memperpanjang Masa Aktif Akun Vmess" | lolcat
+echo -e "* cekvmess          : Cek User Login V2RAY" | lolcat
+echo -e "* cert2vray         : Memperbarui Sertifikat Akun V2RAY [Apabila Ganti Domain]" | lolcat
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━ VLESS ━━━━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "* addvless          : Membuat Akun V2RAY Vless Websocket"
-echo -e "* delvless          : Menghapus Akun V2RAY Vless Websocket"
-echo -e "* renewvless        : Memperpanjang Masa Aktif Akun Vless"
-echo -e "* cekvless          : Cek User Login V2RAY"
+echo -e "* addvless          : Membuat Akun V2RAY Vless Websocket" | lolcat
+echo -e "* delvless          : Menghapus Akun V2RAY Vless Websocket" | lolcat
+echo -e "* renewvless        : Memperpanjang Masa Aktif Akun Vless" | lolcat
+echo -e "* cekvless          : Cek User Login V2RAY" | lolcat
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━ Trojan ━━━━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "* addtrojan         : MEMBUAT Akun Trojan"
-echo -e "* deltrojan         : Menghapus Akun Trojan"
-echo -e "* renewtrojan       : Memperpanjang Masa Aktif Trojan"
-echo -e "* cektrojan.        : Cek User Login Trojan"
+echo -e "* addtrojan         : MEMBUAT Akun Trojan" | lolcat
+echo -e "* deltrojan         : Menghapus Akun Trojan" | lolcat
+echo -e "* renewtrojan       : Memperpanjang Masa Aktif Trojan" | lolcat
+echo -e "* cektrojan.        : Cek User Login Trojan" | lolcat
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━ SYSTEM ━━━━━━━━━━━━━━━━━━━━" | lolcat
-echo -e "* addhost      : Mengganti Domain [Bagi yang memiliki domain]"
-echo -e "* newhost      : Menambahkan Subdomain Baru [Bagi Yang Tidak Memilki Domain]"
-echo -e "* ram          : Cek Penggunaan Ram Dari Vps"
-echo -e "* reboot       : Reboot VPS"
-echo -e "* speedtest    : Speedtest VPS"
-echo -e "* info         : Menampilkan Informasi Vps"
-echo -e "* vnstat       : Menampilkan Penggunaan Bandwidth Vps"
-echo -e "* about        : Info Script Auto Install"
-echo -e "* exit         : Logout Dari VPS "
+echo -e "* addhost      : Mengganti Domain [Bagi yang memiliki domain]" | lolcat
+echo -e "* newhost      : Menambahkan Subdomain Baru [Bagi Yang Tidak Memilki Domain]" | lolcat
+echo -e "* ram          : Cek Penggunaan Ram Dari Vps" | lolcat
+echo -e "* reboot       : Reboot VPS" | lolcat
+echo -e "* speedtest    : Speedtest VPS" | lolcat
+echo -e "* info         : Menampilkan Informasi Vps" | lolcat
+echo -e "* vnstat       : Menampilkan Penggunaan Bandwidth Vps" | lolcat
+echo -e "* about        : Info Script Auto Install" | lolcat
+echo -e "* exit         : Logout Dari VPS " | lolcat
 echo -e ""
 echo -e "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━" | lolcat
 echo -e "AutoScript By JAGOANNEON" | lolcat
