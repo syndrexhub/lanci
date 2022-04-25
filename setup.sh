7e9fa281644feb084dad146945bd8704af4d592c
jagoanneon01 <89620451+jagoanneon01@users.noreply.github.com>
2022-04-26 03:35:33 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -42,7 +42,7 @@ wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/startcomand.sh &
 #install xray
 wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/xray/ins-xray.sh && chmod +x ins-xray.sh && sed -i -e 's/\r$//' ins-xray.sh && screen -S xray ./ins-xray.sh
 # Trojango
-wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/trojango/ins-trojango.sh && chmod +x ins-trojango.sh && sed -i -e 's/\r$//' ins-trojango.sh && ./ins-trojango.sh
+#wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/trojango/ins-trojango.sh && chmod +x ins-trojango.sh && sed -i -e 's/\r$//' ins-trojango.sh && ./ins-trojango.sh
 # install xray grpc
 wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/grpc/grpc.sh && chmod +x grpc.sh && sed -i -e 's/\r$//' grpc.sh && screen -S grpc ./grpc.sh
 #
@@ -52,7 +52,7 @@ wget https://raw.githubusercontent.com/jagoanneon01/njajal/main/websocket/edu.sh
 
 rm -f /root/startcomand.sh
 rm -f /root/ins-xray.sh
-rm -f /root/ins-trojango.sh
+#rm -f /root/ins-trojango.sh
 rm -f /root/grpc.sh
 rm -f /root/edu.sh
 history -c
