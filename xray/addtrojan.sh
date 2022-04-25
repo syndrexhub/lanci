2f1e0a3f459bfb02c3cd158e43a8a9996838ffb4
jagoanneon01 <89620451+jagoanneon01@users.noreply.github.com>
2022-04-26 05:32:00 +0700

    Update addtrojan.sh

--- xray/addtrojan.sh
+++ xray/addtrojan.sh
@@ -46,19 +46,12 @@ until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${user_EXISTS} == '0' ]]; do
 read -p "Expired (Days) : " masaaktif
 hariini=`date -d "0 days" +"%Y-%m-%d"`
 exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
-sed -i '/#xray-trojan-wstls$/a\#&# '"$user $exp"'\
-},{"password": "'""$user""'","email": "'""$user""'"' /etc/xray/trojan.json
-sed -i '/#xray-trojan-wsnone$/a\#&# '"$user $exp"'\
-},{"password": "'""$user""'","email": "'""$user""'"' /etc/xray/trojan.json
 sed -i '/#xray-trojan$/a\#&# '"$user $exp"'\
 },{"password": "'""$user""'","email": "'""$user""'"' /etc/xray/config.json
 sed -i '/#trojangrpc$/a\#&# '"$user $exp"'\
 },{"password": "'""$user""'","email": "'""$user""'"' /etc/xray/trojangrpc.json
 systemctl restart xray.service
 trojanlink="trojan://${user}@${domain}:${tr}/${bug}"
-link="trojan://${user}@${bug}:2053/?sni=${domain}&type=ws&host=${domain}&path=/Ronggolawe&encryption=none#$user"
-link2="trojan://${user}@${bug}:2080/?type=ws&host=${domain}&path=/Ronggolawe&encryption=none#$user"
-link3="trojan://${user}@${bug}:3443/?mode=multi&security=tls&encryption=none&type=grpc&serviceName=GunService&sni=${domain}#$user"
 service cron restart
 clear
 echo -e ""
@@ -69,9 +62,7 @@ echo -e "Remarks     : ${user}" | lolcat
 echo -e "IP/Host     : ${MYIP}" | lolcat
 echo -e "Address     : ${domain}" | lolcat
 echo -e "Port TCP    : ${tr}" | lolcat
-echo -e "Port GRPC   : 3443" | lolcat
-echo -e "Port WSTLS  : 2053" | lolcat
-echo -e "Port WSNONE : 2080" | lolcat
+echo -e "Port GRPC   : 8880" | lolcat
 echo -e "Network     : TCP & Grpc" | lolcat
 echo -e "Mode        : Multi" | lolcat
 echo -e "SecurityGRPC: TLS" | lolcat
@@ -84,12 +75,6 @@ echo -e ">>>> ${trojanlink}" | lolcat
 echo -e "════════════════" | lolcat
 echo -e "Link TR GRPC  : " | lolcat
 echo -e ">>>> ${link3}" | lolcat
-echo -e "════════════════" | lolcat
-echo -e "Link TR WSTLS  : " | lolcat
-echo -e ">>>> ${link}" | lolcat
-echo -e "════════════════" | lolcat
-echo -e "Link TR WSNONE  : " | lolcat
-echo -e ">>>> ${link2}" | lolcat
 echo -e "═══════════════════════" | lolcat
 echo -e "Created     : $hariini" | lolcat
 echo -e "Expired     : $exp" | lolcat
